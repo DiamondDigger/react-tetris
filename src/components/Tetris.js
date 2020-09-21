3bae941ebe7af60638ae436a0900fc77f9eadf26
naglec13 <naglec13@gmail.com>
2020-09-22 00:32:14 +0300

    - #3 - added frames for all components

---
+++ src/components/Tetris.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Stage from './Stage'
+import Cell from './Cell'
+import Display from './Display'
+import StartButton from './StartButton'
+
+function Tetris() {
+
+    return (
+        <div>
+            <Stage/>
+            <aside>
+                <div>
+                    <Display text='Score'/>
+                    <Display text='Rows'/>
+                    <Display text='Level'/>
+                </div>
+            </aside>
+            <StartButton/>
+        </div>
+
+    )
+}
+
+export default Tetris
