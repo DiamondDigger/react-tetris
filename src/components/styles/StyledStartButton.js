f1c36017e1ae331b0a9a61cda6207ddaaf3ead1f
naglec13 <naglec13@gmail.com>
2020-09-23 13:43:12 +0300

    - #10 - added StyledStartButton.js with style for StartButton

---
+++ src/components/styles/StyledStartButton.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const StyledStartButton = styled.button`
+    box-sizing: border-box;
+    display: flex;
+    width: 100%;
+    min-height: 30px;
+    padding: 20px;
+    margin: 0 0 20px 0;
+    border: none;
+    border-radius: 20px;
+    background: #333;
+    color: white;
+    font-family: Pixel, Arial, Helvetica, sans-serif;
+    font-size: 0.8rem;
+    outline: none;
+    cursor: pointer;
+`
