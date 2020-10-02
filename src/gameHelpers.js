9748f9e8fd826e0d366400a2a37efae007e15395
naglec13 <naglec13@gmail.com>
2020-10-02 11:26:29 +0300

    - #25 - added function breakpoints structure

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -6,3 +6,10 @@ export function createStage() {
         new Array(STAGE_WIDTH).fill([0, 'clear'])
     )
 }
+
+export const checkCollision = (stage, player, {x: moveX, y: moveY}) => {
+    //1. check cell of tetromino
+    //2. check that we are not go outside of the stage in vertical dimension  (y)
+    //3. check that we are not go outside of the stage in horizontal dimension (x)
+    //4. check the value of the next cell( in which we move to) - 'clear' or not
+}
