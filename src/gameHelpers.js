5b8adce45e0d1e52eb6083ed105635cce3f65fee
naglec13 <naglec13@gmail.com>
2020-10-02 12:13:42 +0300

    - #25 - added logic

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -8,8 +8,20 @@ export function createStage() {
 }
 
 export const checkCollision = (stage, player, {x: moveX, y: moveY}) => {
-    //1. check cell of tetromino
-    //2. check that we are not go outside of the stage in vertical dimension  (y)
-    //3. check that we are not go outside of the stage in horizontal dimension (x)
-    //4. check the value of the next cell( in which we move to) - 'clear' or not
+    for (let y = 0; y < player.tetromino.length; y++) {
+        for (let x = 0; x < player.tetromino[0].length; x++) {
+            //1. check cell of tetromino with a shape
+            if (player.tetromino[y][x] !== 0) {
+                if (
+                    //2. check that we are not go outside of the stage in vertical dimension  (y)
+                    !stage[y + player.pos.y + moveY] ||
+                    //3. check that we are not go outside of the stage in horizontal dimension (x)
+                    !stage[y + player.tetromino[y] + moveY][x + player.tetromino[y][x] + moveX] ||
+                    //4. check the value of the next cell( in which we move to) - 'clear' or not
+                    stage[y + player.tetromino[y] + moveY][x + player.tetromino[y][x]][1] !== 'clear'
+                )
+                    return false
+            }
+        }
+    }
 }
