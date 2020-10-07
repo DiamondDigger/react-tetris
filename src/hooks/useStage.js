9987a8cd59f09ffa095d852d76923e5a78e21800
naglec13 <naglec13@gmail.com>
2020-10-07 14:14:32 +0300

    - #29 - added logic structure for the new function

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,12 +4,17 @@ import {createStage} from "../gameHelpers";
 
 export function useStage(player, resetPlayer) {
     const [stage, setStage] = useState(createStage());
+    //1. add another state for the sweet row
 
     useEffect(() => {
         const updateStage = prevStage => {
             // First flush the stage
             const newStage = prevStage.map(row =>
                 row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)))
+
+            //2. check if the row is full
+            //3. flush it with .reduce of the newStage
+            //4. call the function when you check player collided option
 
             // Then draw the tetromino
             player.tetromino.forEach((row, y) => {
