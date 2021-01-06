8d616d2b6d17695edc99635a8812b5b1c01f3453
naglec13 <naglec13@gmail.com>
2021-01-07 01:37:39 +0300

    - #10- add media query

--- src/components/styles/StyledStartButton.js
+++ src/components/styles/StyledStartButton.js
@@ -1,18 +1,26 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const StyledStartButton = styled.button`
-    box-sizing: border-box;
-    display: flex;
-    width: 100%;
-    min-height: 30px;
-    padding: 20px;
-    margin: 0 0 20px 0;
-    border: none;
-    border-radius: 20px;
-    background: #333;
-    color: white;
-    font-family: Pixel, Arial, Helvetica, sans-serif;
-    font-size: 0.8rem;
-    outline: none;
-    cursor: pointer;
-`
+  box-sizing: border-box;
+  display: flex;
+  width: 100%;
+  min-height: 30px;
+  padding: 20px;
+  margin: 0 0 20px 0;
+  border: none;
+  border-radius: 20px;
+  background: #333;
+  color: white;
+  font-family: Pixel, Arial, Helvetica, sans-serif;
+  font-size: 0.8rem;
+  outline: none;
+  cursor: pointer;
+
+  @media screen and (max-width: 600px) {
+    width: 50%;
+    min-height: 20px;
+    padding: 10px;
+    font-size: 0.6rem;
+    justify-content: center;
+  }
+`;
